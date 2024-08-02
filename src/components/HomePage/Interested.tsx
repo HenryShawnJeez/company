b74f76c56751c416851290344f9f419ee8e2a375
Priest <Charleschukwuemeka47@gmail.com>
2024-08-02 18:48:37 +0100

    Fix: Change the interest title

--- src/components/HomePage/Interested.tsx
+++ src/components/HomePage/Interested.tsx
@@ -18,7 +18,7 @@ const Interested = () => {
     return ( 
         <main className="relative z-10 text-center px-5 sm:px-10 md:px-20 xl:px-32 -mt-10 ">
             <div className="flex flex-col gap-y-5 py-10 p-4 md:p-8 xl:p-10 border border-gray-300 dark:border-gray-600 bg-bgLight text-textLight dark:bg-bgDark dark:text-textDark">
-               <h1 className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-semibold">Looking to Sell Your Oil and Gas Efficiently?</h1> 
+               <h1 className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-semibold">Looking to buy oil and gas from reputable sellers?</h1> 
                <p className="font-medium">Fill out the following details and we will have someone get back to you as soon as possible...</p>
                <form className="flex flex-wrap justify-center xl:justify-between gap-2 mt-4 w-full">
                     <input type="text" value={firstName} onChange={(e: ChangeEvent<HTMLInputElement>) => setFirstName(e.target.value)} required name="firstName" id="firstName" className="min-w-[14rem] sm:w-[18rem] md:w-[23%] 2xl:w-[25%] px-5 py-3 md:py-4 bg-[#F0F0F0] dark:bg-black focus:outline-none caret-primary" placeholder="First Name"/>
